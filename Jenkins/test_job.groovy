84ba3e0567ff0430ee5d615c7f3af4c6067673bb
Roman <roman@somedomain.net>
2022-05-11 21:36:13 +0300

    Good test verison
    Start:
    ansible-playbook -vvv -i Ansible/inventories/inventory Ansible/playbooks/playbook.yml -e "ansible_user=ansible ansible_become_password=$PASSWORD ansible_password=$PASSWORD"

---
+++ Jenkins/test_job.groovy
@@ -0,0 +1,45 @@
+pipeline {
+    agent any
+    parameters { 
+        booleanParam(name: 'REMOVE_NGINX', defaultValue: false, description: 'Do you want to remove Nginx?')
+        string(name: 'GIT_BRANCH', defaultValue: 'develop', description: '')
+    }
+    stages {
+        stage('Clone Git-repo') {
+            steps {
+                script {
+                    cleanWs()
+                    sh "git clone -b $GIT_BRANCH https://github.com/romchegue/tests.git"
+                    sh "ls -l tests"
+                }
+            }
+        }
+        stage('Ansible') {
+            steps {
+                script {
+                    dir('tests') {
+                        sh "pwd"
+                        sh "id"
+                        withCredentials([usernamePassword(credentialsId: 'ansible_user', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                            ansiblePlaybook(
+                                inventory: 'Ansible/inventories/test_hosts',
+                                playbook: 'Ansible/playbooks/playbook.yml',
+                                extras: '-e "ansible_user=$USERNAME' +
+                                        ' ansible_password=$PASSWORD' +
+                                        ' ansible_become_password=$PASSWORD' +
+                                        ' remove_nginx_package=$REMOVE_NGINX" -vvv',
+                                disableHostKeyChecking: true,
+                                colorized: true
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            cleanWs()
+        }
+    }
+}

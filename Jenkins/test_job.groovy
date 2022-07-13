391391afa9c4357100109c87f0aba1a67526c757
Roman <roman@somedomain.net>
2022-07-13 10:07:36 +0300

    Add Ansible-vault feature to Jenkins file

--- Jenkins/test_job.groovy
+++ Jenkins/test_job.groovy
@@ -1,50 +1,58 @@
-pipeline {
-    agent any
-    options {
-        ansiColor('xterm')
-        buildDiscarder(logRotator(numToKeepStr: '7', daysToKeepStr: '10', artifactNumToKeepStr: '3', artifactDaysToKeepStr: '7'))
-    }
-    parameters { 
-        booleanParam(name: 'REMOVE_NGINX', defaultValue: false, description: 'Do you want to remove Nginx?')
-        string(name: 'BRANCH', defaultValue: 'develop', description: '')
-    }
-    stages {
-        stage('Clone Git-repo') {
-            steps {
-                script {
-                    cleanWs()
-                    sh "git clone -b $BRANCH https://github.com/romchegue/tests.git"
-                    sh "ls -l tests"
+node {
+    timeout(time: 30, unit: 'MINUTES') {
+        env.vaultPassFile = "vaultpass.txt"
+        env.gitRepoUrl = "https://github.com/romchegue/tests.git"
+        currentBuild.result = "SUCCESS"
+        try {
+            properties([
+                parameters([
+                    booleanParam(name: 'REMOVE_NGINX', defaultValue: false, description: 'Do you want to remove Nginx?'),
+                    string(name: 'BRANCH', defaultValue: 'develop', description: 'Branch for repository with Ansible code: https://github.com/romchegue/tests.git')
+                ])
+            ])
+            stage('Clone Git-repo') {
+                cleanWs()
+                checkout([
+                    $class: 'GitSCM',
+                    branches: [[name: BRANCH]],
+                    extensions: [],
+                    userRemoteConfigs: [[url: env.gitRepoUrl]]
+                ])
+                sh "ls -la"
+            }
+            stage('Ansible') {
+                prepareVaultPassFile()
+                ansiColor('xterm') {
+                    ansiblePlaybook(
+                        inventory: "${WORKSPACE}/Ansible/inventories/test_hosts",
+                        playbook: "${WORKSPACE}/Ansible/playbooks/playbook.yml",
+                        extras: "-e \"workspace=${WORKSPACE}"+
+                                " remove_nginx_package=$REMOVE_NGINX\" -vv" +
+                                " --vault-password-file ${WORKSPACE}/${env.vaultPassFile}",
+                        disableHostKeyChecking: true,
+                        colorized: true
+                    )
                 }
             }
-        }
-        stage('Ansible') {
-            steps {
-                script {
-                    dir('tests') {
-                        sh "pwd"
-                        sh "id"
-                        withCredentials([usernamePassword(credentialsId: 'ansible_user', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-                            ansiblePlaybook(
-                                inventory: 'Ansible/inventories/test_hosts',
-                                playbook: 'Ansible/playbooks/playbook.yml',
-                                extras: '-e "workspace=${WORKSPACE}' +
-                                        ' ansible_user=$USERNAME' +
-                                        ' ansible_password=$PASSWORD' +
-                                        ' ansible_become_password=$PASSWORD' +
-                                        ' remove_nginx_package=$REMOVE_NGINX" -vvv',
-                                disableHostKeyChecking: true,
-                                colorized: true
-                            )
-                        }
-                    }
-                }
-            }
-        }
-    }
-    post {
-        always {
+        } catch (e) {
+            currentBuild.result = "FAILURE"
+            echo "[ERROR] Build failed"
+            throw e
+        } finally {
             cleanWs()
+        }
+    }
+}
+
+
+private void prepareVaultPassFile() {
+    echo "[INFO] Function prepareVaultPassFile()"
+    withCredentials([file(credentialsId: 'ansible_vault_pass_file', variable: 'FILE')]) {
+        wrap([$class: "MaskPasswordsBuildWrapper"]) {
+            sh """
+                cat $FILE > ${env.vaultPassFile}
+                ls -l ${env.vaultPassFile}
+            """
         }
     }
 }
